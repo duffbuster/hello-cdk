b8537f7442f65628f7544666db00598e3676e92c
Colin Mackey <cmackey@captechconsulting.com>
2019-07-29 09:42:11 -0400

    Prepared for demo

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 
 import core = require('@aws-cdk/core');
 
-import { HelloCdkStack } from '../lib/hello-cdk-stack';
+// import {  } from '../lib/hello-cdk-stack';
 
 const app = new core.App();
-new HelloCdkStack(app, 'HelloCdkStack');
+// new (app, '');
